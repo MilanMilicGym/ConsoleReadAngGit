222c0f90aacd0c22d85afa530e57ea77f105d87a
MilanMilicGym <milanmilicgym@gmail.com>
2022-03-18 11:40:01 +0100

    Initial commit

---
+++ ConsoleReadAngGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleReadAngGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
