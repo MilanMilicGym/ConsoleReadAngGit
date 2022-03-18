b8419205c519f1cc423cb9b361040bee7c74d1d8
MilanMilicGym <milanmilicgym@gmail.com>
2022-03-18 12:01:23 +0100

    Dodato sabiranje

--- ConsoleReadAngGit/Program.cs
+++ ConsoleReadAngGit/Program.cs
@@ -6,7 +6,17 @@ namespace ConsoleReadAngGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Unesite prvi broj: ");
+            int prviBroj = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite drugi broj: ");
+            int drugiBroj = int.Parse(Console.ReadLine());
+
+            int rezultatSabiranja = prviBroj + drugiBroj;
+            Console.WriteLine("Rezultat sabiranja: " + rezultatSabiranja);
+
+            //TODO: Unaprediti ovaj primer da ispisuje rezultate osnovnih matematickih operacija ( + - / *)
+            //TODO: Uraditi commit izmena i publishovati na Git Repo
         }
     }
 }
